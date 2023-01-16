ef24190d6c3136ce28db2b5bd0ba3d1e6748d623
pkrodrigues <pkjrodrigues@gmail.com>
2023-01-16 09:49:37 -0300

    created o métado PUT

--- src/main/java/com/imclpiv/imc/controller/UsuarioController.java
+++ src/main/java/com/imclpiv/imc/controller/UsuarioController.java
@@ -4,10 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -30,11 +31,31 @@ public class UsuarioController {
     public List<Usuario> list(){
         return usuarioRepository.findAll();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getByIdUsuario(@PathVariable Long id) {
+        return usuarioRepository.findById(id)
+        .map(record -> ResponseEntity.ok().body(record))
+        .orElse(ResponseEntity.notFound().build());
+    }
+
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
+   
     public void create(@RequestBody Usuario usuario){
         usuarioRepository.save(usuario);
-        // return ResponseEntity.status(HttpStatus.CREATED)
-        // .body(usuarioRepository.save(usuario));
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity atualizar(@PathVariable("id") Long id, @RequestBody Usuario usuario) {
+        return usuarioRepository.findById(id)
+        .map(recordFound -> {
+            recordFound.setEmail(usuario.getEmail());
+            recordFound.setNome(usuario.getNome());
+            Usuario updated = usuarioRepository.save(recordFound);
+            return ResponseEntity.ok().body(updated);
+        })
+        .orElse(ResponseEntity.notFound().build());
+    }
+
 }
